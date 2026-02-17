e47eb2ce0d42cae9712d8f0b8713d43d07adf2ca
Taketake2145 <simplesimplesdesign@gmail.com>
2026-02-17 09:43:09 +0900

    初期設定

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# photowalk
+# 作成
+githubでリポジトリを作成して、ローカルにクローンする
+_templateディレクトリを複製して、コンテンツの中身をクローンしたディレクトリ内に移動する
+docker-compose.yml のportsの値を変更する（表示用とDB用の2箇所）
+
+共通用 commonディレクトリ
+sync_watch.sh にパスを追加する
+
+# 運用
+
+## 作業開始時
+docker compose up -d && dgulp
+
+これで gulp の watch が始まる
+
+## 作業終了時
+1. control + c これで watch（監視）が終了する
+2. docker compose stop もしくは GUI で停止ボタンを選択する
---
+++ www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <meta name="description" content="">
+  <link rel="stylesheet" href="/assets/css/common.min.css?<?php echo filemtime('./assets/css/common.min.css'); ?>">
+</head>
+<body>
+  <script src="/assets/js/jquery-3.7.1.min.js"></script>
+  <script src="/assets/js/common.min.js?<?php echo filemtime('./assets/js/common.min.js'); ?>"></script>
+</body>
+</html>
